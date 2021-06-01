d2bf1843abc9718179ddfea22baf9a6d06908a10
Ian King <kickthechair@gmail.com>
2021-06-01 15:25:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+ if int.Prime?
+   true
+  else
+    false
+  end
+end
+
+
+
+#return true if int is a prime number
+#return false if int not a prime number
+
+#Prime numbers are special numbers, greater than 1, that have exactly two factors, themselves and 1.
+
+    #19 is a prime number. It can only be divided by 1 and 19.
+    #9 is not a prime number. ...
+    #Don't forget: the number 1 is not a prime number as it only has one factor.
